c8d4aff269a034730ac9725cf7f9c12901cade74
Ash <ash@yoomee.com>
2019-04-29 10:54:42 +0100

    Remove secret keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',                        # required
       region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
-      aws_access_key_id:     'AKIA5JCDRIYG75CWKD4F',                        # required unless using use_iam_profile
-      aws_secret_access_key: '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',                        # required unless using use_iam_profile
-      use_iam_profile:       false,                         # optional, defaults to false
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required unless using use_iam_profile
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required unless using use_iam_profile
+      use_iam_profile:       false,                        # optional, defaults to false
     }
     config.fog_directory  = 'myyelpdemo'                                      # required
   end
