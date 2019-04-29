74a92dd502a60933ae8081d6100c2c8fb7de2677
Ash <ash@yoomee.com>
2019-04-29 08:29:52 +0100

    uploader change

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    storage: fog
+    storage :fog
   else
-    storage: file
+    storage :file
   end
   # storage :file
 
