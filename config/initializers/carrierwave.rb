d0a95a688fc193d70521dc972ce9d63b934fa751
Ash <ash@yoomee.com>
2019-04-29 11:01:25 +0100

    Works when no ENV variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
+    aws_access_key_id = ENV['AWS_ACCESS_KEY_ID'] || "blank"
+    aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY'] || "blank"
     config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
       provider:              'AWS',                        # required
       region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required unless using use_iam_profile
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required unless using use_iam_profile
+      aws_access_key_id:     aws_access_key_id,     # required unless using use_iam_profile
+      aws_secret_access_key: aws_secret_access_key, # required unless using use_iam_profile
       use_iam_profile:       false,                        # optional, defaults to false
     }
     config.fog_directory  = 'myyelpdemo'                                      # required
