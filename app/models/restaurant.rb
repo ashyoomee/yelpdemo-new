dd4705f688f1e097683bed21aa92ad7596d1192f
Ash <ash@yoomee.com>
2019-05-03 14:09:35 +0100

    adding stars

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
     mount_uploader :image, ImageUploader
+
+    has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews, except:[:show, :index]
   devise_for :users
-  resources :restaurants
-  get 'pages/about'
+  resources :restaurants do  
+    resources :reviews, except:[:show, :index]
+  end
+  
+    get 'pages/about'
   get 'pages/contact'
   root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
