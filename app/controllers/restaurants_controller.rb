45d7ee73e7bf903f236904ad83722276e515254e
Ash <ash@yoomee.com>
2019-05-17 14:37:09 +0100

    Update restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,9 +10,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
-    def show
-      @reviews = Review.where(restaurant_id: @restaurant.id)
-    end
+    @reviews = Review.where(restaurant_id: @restaurant.id)
   end
 
   # GET /restaurants/new
