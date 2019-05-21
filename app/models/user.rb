0836d681c1ff50cb46f29c899b19064d8eaefb9c
Ash <ash@yoomee.com>
2019-05-21 12:35:27 +0100

    Add data validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy     
+
+  validates :first_name, :last_name, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    message: "can only be a whole number between 1 and 5"
+  }
+
 end
 
 
